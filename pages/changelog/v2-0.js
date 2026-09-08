6458a72fd7c01ad5ea9ff9f1f0b22ad4e01733a1
stickman <mhmdfaturrhmn18@gmail.com>
2026-09-08 15:16:40 +0700

    Add changelog entry for version 2.0

---
+++ pages/changelog/v2-0.js
@@ -0,0 +1,6 @@
+CHANGELOG_DATA.push({
+  id: "v2-0",
+  version: "V2.0",
+  date: "30/08/2026",
+  desc: "Perbaikan bug, Mengatur ulang Elemen, Mengatur ulang Animasi, Menata kembali masing-masing Halaman, dan Rekonstruksi."
+});
