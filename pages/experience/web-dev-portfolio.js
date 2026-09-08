4d82fd2c428396fcd8df9146a010e45278077f1f
stickman <mhmdfaturrhmn18@gmail.com>
2026-09-08 15:14:56 +0700

    Add web development portfolio experience entry

---
+++ pages/experience/web-dev-portfolio.js
@@ -0,0 +1,8 @@
+EXPERIENCE_DATA.push({
+  id: "web-dev-portfolio",
+  year: "2026 - Present",
+  title: "Web Development & Portfolio Creator",
+  company: "Independent / School Projects",
+  desc: "Mengembangkan berbagai proyek halaman web statis maupun interaktif secara mandiri dan kolaboratif, menerapkan praktik clean code dan responsive design.",
+  tags: ["HTML5", "CSS3", "JavaScript", "Git"]
+});
